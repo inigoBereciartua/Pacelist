e410612ae76d101f98f0f30afeba0db70539d958
inigoBereciartua <ibereciartua99@gmail.com>
2024-11-01 17:14:39 +0100

    First commit backend setup

---
+++ backend/src/test/java/com/ibereciartua/pacelist/PacelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibereciartua.pacelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
